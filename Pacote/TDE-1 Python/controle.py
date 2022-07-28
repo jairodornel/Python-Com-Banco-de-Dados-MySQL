78792288a5cf2aa644709441be3698deed1962c7
jairodornel <89214328+jairodornel@users.noreply.github.com>
2022-07-27 21:26:15 -0300

    Alteração senha

--- Pacote/TDE-1 Python/controle.py
+++ Pacote/TDE-1 Python/controle.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 import mysql.connector
 
-con = mysql.connector.connect(host='localhost', database='cadastro1',user='root',password='Imperador100@')
+con = mysql.connector.connect(host='localhost', database='cadastro1',user='root',password='*******')
 
 if con.is_connected():
     db_info = con.get_server_info()
